f68a8fe1878a08011080015b777a978bce30412b
Alex <alex.beciana@gmail.com>
2022-02-23 20:04:31 -0500

    Generated models and created db

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  resources :notes
+  resources :notebooks
+  resources :tasks
+  resources :homeworks
+  resources :meetings
+  resources :lectures
+  resources :courses
+  resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
