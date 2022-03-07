047f092aa1b85312ed57673b98dcda7194952222
Alex <alex.beciana@gmail.com>
2022-03-07 15:26:37 -0500

    Deleted factories direct and content

--- app/models/course.rb
+++ app/models/course.rb
@@ -2,7 +2,7 @@ class Course < ApplicationRecord
     belongs_to :teacher, :foreign_key => "teacher_id", :class_name => "User"
     has_many :cohorts
     has_many :students, through: :cohorts
-    has_many :homeworks
+    # has_many :homeworks
 
     validates :name, presence: true
     validates :subject, presence: true
