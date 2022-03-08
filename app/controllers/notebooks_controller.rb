7934926ccb7c78e3f6833ca21d129bac0e7bce67
Alex <alex.beciana@gmail.com>
2022-03-07 19:16:17 -0500

    Completed notebook request tests

--- app/controllers/notebooks_controller.rb
+++ app/controllers/notebooks_controller.rb
@@ -1,2 +1,38 @@
+require 'pry'
+
 class NotebooksController < ApplicationController
+
+    def show
+        @notebook = Notebook.find(params[:id])
+        render json: @notebook
+    end
+
+    def create
+        @notebook = Notebook.new(notebook_params)
+        if @notebook.valid?
+            @notebook.save
+            render json: @notebook, status: :created
+        else
+            render json: @notebook.errors, status: :unprocessable_entity
+        end
+    end
+
+    def update
+        @notebook = Notebook.find(params[:id])
+        @notebook.update(notebook_params)
+        render json: @notebook
+    end
+
+    def destroy
+        @notebook = Notebook.find(params[:id])
+        @notebook.destroy
+        render json: {}
+    end
+
+    private
+
+    def notebook_params
+        params.require(:notebook).permit(:heading, :description, :user_id)
+    end
+
 end
