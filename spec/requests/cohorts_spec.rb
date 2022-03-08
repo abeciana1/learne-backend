738e8e0a965d0c5f9fe74c3c62089a1d30effb5b
Alex <alex.beciana@gmail.com>
2022-03-07 22:20:50 -0500

    Complete cohort request testing

--- spec/requests/cohorts_spec.rb
+++ spec/requests/cohorts_spec.rb
@@ -1,7 +1,55 @@
 require 'rails_helper'
 
 RSpec.describe "Cohorts", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+
+  context "CRUD actions" do
+
+    before(:all) do
+      @cohort = Cohort.last
+    end
+
+    describe 'CREATE' do
+      it "should create a new cohort" do
+        expect { 
+          post "/cohorts", params: {
+            cohort: {
+              student_id: 12,
+              course_id: 4
+            }
+          }
+        }.to change {Cohort.count}.by(1)
+        expect(response).to have_http_status(:created)
+      end
+    end
+    
+    describe 'READ' do
+      it "should access a cohort" do
+        get "/cohorts/#{@cohort.id}"
+        expect(response).to have_http_status(:success)
+      end
+    end
+    
+    describe 'UPDATE' do
+      it "PUT & PATCH > should update a whole cohort" do
+        put "/cohorts/#{@cohort.id}", params: {
+          cohort: {
+            course_id: 10
+          }
+        }
+        expect(response).to have_http_status(:success)
+      end
+    end
+
+    describe 'DESTROY' do
+      it "should destroy a cohort" do
+        expect {
+          delete "/cohorts/#{@cohort.id}"
+        }.to change {Cohort.count}.by(-1)
+        expect(response).to have_http_status(:success)
+      end
+    end
+    
+    
+
   end
 end
